7e79eca5d6268ae9ed9ef7b74e6b2a9fdb00135c
aleksmazur <markovasasha94@gmail.com>
2022-06-22 03:51:48 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+    let arrN = n.toString().split('');
+    let newArrN = arrN.reverse();
+    let m = newArrN.join('');
+    return m;
 }
+
+
+// function reverse (n) {
+//     let arrN = n.toString().split('');
+//     console.log(arrN);
+//     let newArrN = arrN.reverse();
+//     console.log(newArrN);
+//     let m = newArrN.join('');
+//     console.log(m);
+//   }
+
+//   reverse(57963);
